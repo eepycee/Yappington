e9ced56df0c8032fbdea54a9dbd1e7f342cf656a
src-ry <imjimothy@proton.me>
2026-01-07 16:31:59 -0500

    feat: add more yes answers there was an insane amount of NOs, which due to basic math, made pretty much every answer no; this change evens it out more (10 yes, 9 nos if im counting correctly)

--- src/commands/eightball.js
+++ src/commands/eightball.js
@@ -3,12 +3,12 @@ const {
   SlashCommandBuilder,
 } = require("discord.js");
 
-let answer = ["yes", "maybe", "indeed", "fuck no", "nah", "dawg what the fuck?", "idk but im gay", "I hate you guys", "I would rather die than answer that question", "I guess", "ask your mother", "due to unfortunate circumcisions, I do not care enough to answer this question.", "my lawyers advised me against answering that question"];
+let answer = ["yes", "maybe", "indeed", "fuck no", "nah", "dawg what the fuck?", "idk but im gay", "I hate you guys", "I would rather die than answer that question", "I guess", "ask your mother", "due to unfortunate circumcisions, I do not care enough to answer this question.", "my lawyers advised me against answering that question", "FUCK YEAH:bangbang:", "is grass green?", "*mmmfgpjghhh yesss~*", "only on tuesdays", "idk but i love you", "god gave you free will"];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ask-me")
-    .setDescription("Ask KuromiBot anything")
+    .setDescription("Ask Yappington anything")
     .addStringOption(option => option
     .setName('question')
 			  .setDescription('Your stupid question!')
@@ -20,7 +20,7 @@ module.exports = {
       .setColor("#5865f4")
       .setTitle(`${question}`)
       .addFields({
-        name: "**KuromiBot says**",
+        name: "**Yappington says**",
         value: `> ${answer[(Math.floor(Math.random() * answer.length))]}`,
         inline: false,
       })
